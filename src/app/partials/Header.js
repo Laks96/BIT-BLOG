fd708b76e7c00269fcc78ccfc3ff02fe3c36e0a1
BIT Student <classroom@bgit.rs>
2019-12-03 11:10:09 +0100

    Also adding Header

---
+++ src/app/partials/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Header = (props) => {
+
+    return <>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">BitBlog</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Home</a></li>
+                    <li><a href="badges.html">Authors</a></li>
+                    <li><a href="collapsible.html">About</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
+
+
+    </>
+
+}
+
+
+export default Header;
